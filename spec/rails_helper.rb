dd86d1c9b84a8e978312db60b183ffd6c74bed4f
FragozoLeonardo <leo.fragozo.dev@gmail.com>
2025-04-19 22:30:37 -0300

    Fix bugs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,16 @@
+require File.expand_path('../config/environment', __dir__)
+require 'rspec/rails'
+require 'factory_bot_rails'
+require 'shoulda/matchers'
+
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+end
+
+
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
     with.library :rails
   end
-end
-
-RSpec.configure do |config|
-  config.include FactoryBot::Syntax::Methods
 end
